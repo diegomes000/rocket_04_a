d8ac216720144558dfaffff8530366701ddd808c
Diego Gomes <diegomes000@gmail.com>
2023-06-26 11:39:54 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+
+let a = prompt('Type here a first number');
+let b = prompt('Type here a second number');
+
+let number1 = parseInt(a);
+let number2 = parseInt(b);
+
+
+console.log(`The addition will be ${number1 + number2}`);
+console.log(`The subtraction will be ${number1 - number2}`);
+console.log(`The multiplication will be ${number1 * number2}`);
+console.log(`The division will be ${number1 / number2}`);
+console.log(`The the rest of the division will be ${number1 % number2}`);
+
